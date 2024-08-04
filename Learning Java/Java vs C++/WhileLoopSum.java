884516d69117a63b0db01a64ae14caa017ebf6b6
Kirisannn <tanshangian@hotmail.com>
2024-08-05 02:24:24 +0930

    Learning Base Java

---
+++ Learning Java/Java vs C++/WhileLoopSum.java
@@ -0,0 +1,12 @@
+
+class WhileLoopSum {
+
+    public static void main(String[] args) {
+        int sum = 0, i = 1;
+        while (i < 11) {
+            sum += i;
+            i++;
+        }
+        System.out.println("The sum is: " + sum);
+    }
+}
